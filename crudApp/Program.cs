23ced05a345526293db4e0d850e8fc4bb8084af7
Ryan <rlsystems@outlook.com>
2024-04-18 23:57:23 -0500

    Add project files.

---
+++ crudApp/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using crudApp.Persistence.Contexts;
+using crudApp.Services.ProductService;
+
+
+// create builder
+var builder = WebApplication.CreateBuilder(args); 
+
+// default services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// add database service with configuration
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// add service
+builder.Services.AddTransient<IProductService, ProductService>();
+
+// build app
+var app = builder.Build(); 
+
+
+// middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection(); 
+app.UseAuthorization(); 
+app.MapControllers();
+
+// run app
+app.Run(); 
+
+
